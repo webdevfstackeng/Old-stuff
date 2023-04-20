4d0fdc53339e16568167634944e58d168a934228
webdevfstackeng <u175797830@byui.edu>
2023-04-20 23:57:16 +0200

    Prep2 commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? " ); // prompt for first name
+        string firstName = Console.ReadLine(); // Read first name
+        
+        Console.Write("What is your last name? "); // prompt for last name
+        string lastName = Console.ReadLine(); // Read last name
+
+        Console.WriteLine(); // Leave space for readability
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}."); // Output fullname to console
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        int response = int.Parse(gradePercentage);
+        string letter = "";
+
+        if (response >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (response >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (response >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (response >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got grade {letter}.");
+
+        if (response >= 70)
+        {
+            Console.WriteLine($"Congratulations!! You passed.");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry you could not make it. Good luck next time.");
+        }
+
+
+
     }
 }
